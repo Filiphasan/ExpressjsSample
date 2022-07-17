6d4004b9ffa079177f986e18e00e89a2e6798e9c
Hasan Erdal <hasaerda@hotmail.com>
2022-07-17 19:24:53 +0300

    sequelize init

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express")
 const dotenv = require("dotenv")
 dotenv.config();
 
+const port = process.env.PORT;
+
 const app = express();
 
-app.listen(3000);
+app.get("/", (req, res) => {
+    res.send("Server is Running...")
+})
+
+app.listen(port, () => {
+    console.log(`App running on http://localhost:${port}`);
+});
